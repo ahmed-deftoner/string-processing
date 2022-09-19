36d8be8c5b0e9971f192af59ebb97d49724b50bd
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-09-19 21:30:34 +0500

    done

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::str::FromStr;
+use std::{str::FromStr};
 
+#[derive(Debug)]
 struct Point{
     x: u64,
     y: u64
 }
 
+#[derive(Debug)]
 enum Fold {
     X(u64),
     Y(u64)
@@ -15,9 +17,10 @@ impl FromStr for Point {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let (x, y) = s.split_once(",").unwrap();
+        println!("{}:{}", x,y);
         return Ok(Point{
             x: x.parse().unwrap(),
-            y: y.parse().unwrap()
+            y: y.parse().unwrap(),
         });
     }
 }
@@ -27,15 +30,38 @@ impl FromStr for Fold {
     type Err = std::string::ParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (axis, value) = s.split_once("fold along").unwrap().1.split_once("=").unwrap();
+        let (axis, value) = s.split_once("fold along")
+        .unwrap().
+        1
+        .split_once("=")
+        .unwrap();
         return match axis {
             "x" => Ok(Fold::X(value.parse().unwrap())),
             "y" => Ok(Fold::Y(value.parse().unwrap())),
-            _ => unreachable!()
+            _ => unreachable!(),
         };
     }
 }
 
 fn main() {
-    
+   // env::set_var("RUST_BACKTRACE", "1");
+    let (points, folds) = include_str!("./sample.in")
+    .trim()
+    .split_once("\n\n")
+    .unwrap();
+    let points: Vec<Point> = points.lines()
+    .map(str::parse)
+    .map(Result::unwrap)
+    .collect();
+    let folds: Vec<Point> = folds.lines()
+    .map(str::parse)
+    .map(Result::unwrap)
+    .collect();
+    for p in points {
+        println!("x : {:?} - y : {:?}", p.x,p.y);
+    }
+
+    for f in folds {
+        println!("{:?}", f);
+    }
 }
